17deb7b11a3924944f145a219f4191e9df22a7c4
Alexiotovv <gpsalexvasquez@gmail.com>
2024-08-20 08:20:48 -0500

    seeder_ejecutar_db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,17 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Seeder;
 use DB;
 use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
+
+
         DB::table('users')->insert([
             'name'=>'admin',
             'email'=>'admin@gmail.com',
@@ -37,23 +36,28 @@ class DatabaseSeeder extends Seeder
             ['codigo' => '02','nombre'=>'JURÍDICO']
         ]);
 
+        $procedureExists = DB::select("SHOW PROCEDURE STATUS WHERE Db = DATABASE() AND Name = 'sp_scc_pagos'");
 
+        if (empty($procedureExists)) {
 
-        DB::statement("
-            CREATE PROCEDURE sp_scc_pagos()
-            BEGIN
-                SELECT 
-                    YEAR(pagos.fecha) AS anho,
-                    MONTH(pagos.fecha) AS mes_numero,
-                    MONTHNAME(pagos.fecha) AS mes_nombre,
-                    SUM(pagos.monto) AS total_pagos
-                FROM pagos
-                WHERE pagos.fecha >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
-                GROUP BY YEAR(pagos.fecha), MONTHNAME(pagos.fecha), MONTH(pagos.fecha)
-                ORDER BY YEAR(pagos.fecha), MONTH(pagos.fecha) DESC;
-            END
-        ");
+            DB::statement("
+                CREATE PROCEDURE sp_scc_pagos()
+                BEGIN
+                    SELECT 
+                        YEAR(pagos.fecha) AS anho,
+                        MONTH(pagos.fecha) AS mes_numero,
+                        MONTHNAME(pagos.fecha) AS mes_nombre,
+                        SUM(pagos.monto) AS total_pagos
+                    FROM pagos
+                    WHERE pagos.fecha >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
+                    GROUP BY YEAR(pagos.fecha), MONTHNAME(pagos.fecha), MONTH(pagos.fecha)
+                    ORDER BY YEAR(pagos.fecha), MONTH(pagos.fecha) DESC;
+                END
+            ");
+        }
 
+        $path = base_path('ubigeos.sql');
+        DB::unprepared(File::get($path));
 
     }
 }
