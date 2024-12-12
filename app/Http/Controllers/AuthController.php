5ed0eec1de2b097c8b1bb3053afecf6dc8618128
Alexiotovv <gpsalexvasquez@gmail.com>
2024-12-12 11:36:27 -0500

    https

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Tymon\JWTAuth\Facades\JWTAuth;
+// use Tymon\JWTAuth\Facades\JWTAuth;
 
 use Validator;
 use App\Models\User;
-use std\Class;
-use Iluminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Hash;
 
 
 class AuthController extends Controller
